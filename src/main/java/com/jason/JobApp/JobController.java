631b8c809d13d07eaacd76b01e047b7e8ef12252
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-05 18:35:18 +0530

    Update JobController.java

--- src/main/java/com/jason/JobApp/JobController.java
+++ src/main/java/com/jason/JobApp/JobController.java
@@ -4,9 +4,12 @@ import com.jason.JobApp.model.JobPost;
 import com.jason.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -28,5 +31,12 @@ public class JobController {
     public String handleForm(JobPost jobPost){
         service.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model m){
+        List<JobPost> jobs = service.getAllJobs();
+        m.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 }
